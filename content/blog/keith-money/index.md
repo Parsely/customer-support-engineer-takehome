5563ab71023f482ea6fe2dd855bacaaa5a63eade
Aakash Shah <mraakashshah@users.noreply.github.com>
2019-08-27 20:28:03 -0400

    Parsley brightens food; Parse.ly brightens content.

---
+++ content/blog/keith-money/index.md
@@ -0,0 +1,14 @@
+---
+author: "Keith's #1 Fan"
+date: "2015-05-06T23:46:37.121Z"
+section:
+tags:
+  - keith
+thumbnail: keithmoney.png
+title: Keith dot Money 💸
+type: Organization
+---
+
+`money` is a valid top-level domain! Check out [keith.money](http://keith.money/) the unofficial blog of [Parse.ly's VP of Engineering](https://www.parse.ly/about/keith_bourgoin/)
+
+Never doubt the genius of Keith: ![Keith Money Emoji](./keithmoney.png)
---
+++ src/templates/blog-post.js
@@ -0,0 +1,139 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Helmet from "react-helmet"
+import { rhythm, scale } from "../utils/typography"
+
+const isBrowser = () => typeof window !== "undefined"
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = this.props.data.markdownRemark
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const { previous, next } = this.props.pageContext
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title={post.frontmatter.title}
+          description={post.frontmatter.description || post.excerpt}
+        />
+        <Helmet>
+          <script type="application/ld+json">
+            {`{"@context": "http://schema.org",
+            "@type": "${post.frontmatter.type}",
+            "headline": "${post.frontmatter.title}",
+            "creator": "${
+              post.frontmatter.author
+                ? post.frontmatter.author
+                : this.props.data.site.siteMetadata.author
+            }",
+            "url": "${isBrowser() && window.location.href}",
+            "thumbnailUrl": "${
+              post.frontmatter.thumbnail
+                ? isBrowser() &&
+                  window.location.href + "" + post.frontmatter.thumbnail
+                : "https://pbs.twimg.com/profile_images/718097917874212865/6qsGC2He.jpg"
+            }",
+            "datePublished": "${post.frontmatter.date}",
+            "articleSection": "${post.frontmatter.section}",
+            "keywords": "${post.frontmatter.tags}"}`}
+          </script>
+        </Helmet>
+        <article>
+          <header>
+            <h1
+              style={{
+                marginTop: rhythm(1),
+                marginBottom: 0,
+              }}
+            >
+              {post.frontmatter.title}
+            </h1>
+            <p
+              style={{
+                ...scale(-1 / 5),
+                display: `block`,
+                marginBottom: rhythm(1),
+              }}
+            >
+              Published {post.frontmatter.date} by{" "}
+              {post.frontmatter.author
+                ? post.frontmatter.author
+                : this.props.data.site.siteMetadata.author}
+            </p>
+          </header>
+          <section dangerouslySetInnerHTML={{ __html: post.html }} />
+          <hr
+            style={{
+              marginBottom: rhythm(1),
+            }}
+          />
+          <footer>
+            <Bio />
+          </footer>
+        </article>
+
+        <nav>
+          <ul
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+            }}
+          >
+            <li>
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </Layout>
+    )
+  }
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        description
+        tags
+        section
+        thumbnail {
+          id
+        }
+        type
+        author
+      }
+    }
+  }
+`
